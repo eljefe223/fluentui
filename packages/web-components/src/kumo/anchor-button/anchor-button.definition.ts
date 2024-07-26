9c21ee4277e619fe105b794efd5b11bcf06ad3fb
Dave Rupert <rupertdavid@microsoft.com>
2024-07-26 15:22:37 -0500

    add new classes that extend base classes

--- packages/web-components/src/kumo/anchor-button/anchor-button.definition.ts
+++ packages/web-components/src/kumo/anchor-button/anchor-button.definition.ts
@@ -1,13 +1,13 @@
-import { AnchorButton } from '../../anchor-button/anchor-button.js';
 import { template } from '../../anchor-button/anchor-button.template.js';
 import { styles } from '../button/button.styles.js';
+import { KumoAnchorButton } from './anchor-button.js';
 
 /**
  * @public
  * @remarks
  * HTML Element: \<kumo-anchor-button\>
  */
-export const definition = AnchorButton.compose({
+export const definition = KumoAnchorButton.compose({
   name: `kumo-anchor-button`,
   template,
   styles,
