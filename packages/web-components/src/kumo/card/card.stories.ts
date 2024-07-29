43b458830e24b5700a749de9fb4a825a90c89389
jeff smith <jes@microsoft.com>
2024-07-29 09:52:26 -0700

    adds card css partial

---
+++ packages/web-components/src/kumo/card/card.stories.ts
@@ -0,0 +1,22 @@
+import { html } from '@microsoft/fast-element';
+import type { Args, Meta } from '@storybook/html';
+import { renderComponent } from '../../helpers.stories.js';
+import { cardStyles } from '../styles/card.partials.js';
+
+type CardStoryArgs = Args;
+type CardStoryMeta = Meta<CardStoryArgs>;
+
+const storyTemplate = html<CardStoryArgs>`
+  <style>
+    .my-card {
+      ${cardStyles.createCSS(() => {})}
+    }
+  </style>
+  <div class="my-card">test</div>
+`;
+
+export default {
+  title: 'Components/Kumo/Card',
+} as CardStoryMeta;
+
+export const Card = renderComponent(storyTemplate).bind({});
