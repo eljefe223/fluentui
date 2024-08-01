908bf4be0e90f5520bd9ccb4a6ee22fd073ed752
jeff smith <jes@microsoft.com>
2024-08-01 16:08:49 -0700

    adds kumo field

--- packages/web-components/src/index-rollup.ts
+++ packages/web-components/src/index-rollup.ts
@@ -45,6 +45,7 @@ import './kumo/button/define.js';
 import './kumo/checkbox/define.js';
 import './kumo/dialog/define.js';
 import './kumo/dialog-body/define.js';
+import './kumo/field/define.js';
 import './kumo/menu-button/define.js';
 import './kumo/radio/define.js';
 import './kumo/radio-group/define.js';
