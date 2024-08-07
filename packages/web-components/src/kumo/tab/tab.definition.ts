53126647f9e3a26f2beb13ed871ee25c88434b78
Dave Rupert <rupertdavid@microsoft.com>
2024-08-07 12:40:09 -0500

    add kumo-tablist

---
+++ packages/web-components/src/kumo/tab/tab.definition.ts
@@ -0,0 +1,16 @@
+import { Tab } from '../../tab/tab.js';
+import { template } from '../../tab/tab.template.js';
+import { styles } from '../../tab/tab.styles.js';
+
+/**
+ * The Kumo Tab element definition.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<kumo-tablist>`
+ */
+export const definition = Tab.compose({
+  name: `kumo-tab`,
+  template,
+  styles,
+});
